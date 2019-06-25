1f5d780b77b1445e4b8f675b171a505727089f58
Emma FitzGerald <emmamariafitzgerald@gmail.com>
2019-06-25 13:39:53 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,13 +1,16 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array)
+  array.each_with_index { |item| puts "#{item}" }
+end
+array = ["Doc", "Dopey", "Bashful", "Grumpy"]
+
+def summon_captain_planet(planeteer_calls)
+  planeteer_calls.collect! { |calls| calls.capitalize + "!" }
+end
+planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
-end
-
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+   array.any? { |word| word.length > 4}
 end
 
 def find_the_cheese# code an argument here

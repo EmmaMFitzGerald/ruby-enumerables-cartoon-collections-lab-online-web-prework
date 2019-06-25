4028ecf6814661aef6587aa01c3f6d3076ed161a
Emma FitzGerald <emmamariafitzgerald@gmail.com>
2019-06-25 13:46:59 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,7 +13,11 @@ def long_planeteer_calls(array)
    array.any? { |word| word.length > 4}
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(array)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+    array.find do |type|
+    cheese_types.include?(type)
+  end 
 end
+
